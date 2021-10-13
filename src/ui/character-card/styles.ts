efc1fb61384ec7c3345a674b8edf02f97d3e056b
Gabriel Pinheiro <gabrielpsilvas@live.com>
2021-10-12 23:23:02 -0300

    feat(Card): increase box-shadow

--- src/ui/character-card/styles.ts
+++ src/ui/character-card/styles.ts
@@ -5,12 +5,12 @@ export const Card = {
   transition: 'transform .3s',
   paddingBottom: '8px',
   '&:hover': {
-    boxShadow: '0 10px 20px rgba(0,0,0, 0.2)',
+    boxShadow: '0 10px 20px rgba(0,0,0, 0.5)',
     transform: 'translateY(-5px)'
   },
   '&:active': {
     transform: 'translateY(-2px)',
-    boxShadow: '0 5px 10px rgba(0,0,0, 0.2)'
+    boxShadow: '0 5px 10px rgba(0,0,0, 0.5)'
   }
 }
 
